f60dbf973c0914a2d57d3f08e27528c5322a8362
kenzotmg <kenzotmg@hotmail.com>
2021-03-05 21:18:41 -0300

    fix env var path

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -18,10 +18,11 @@ public class RestServiceApplication {
     	System.out.println(System.getProperty("user.dir"));
     	System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         options.addArguments("window-size=1920x1080");
         options.addArguments("--disable-gpu");
-        options.setBinary("GOOGLE_CHROME_BIN");
+        options.setBinary(System.getenv("GOOGLE_CHROME_BIN"));
 		WebDriver driver = new ChromeDriver(options);
 		System.out.println("Chrome instance created!");
         return driver;
